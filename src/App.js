879621155eecc8ea5e3d9356343bc27670b18998
ebdem <ebu.bekir.3333@gmail.com>
2020-11-22 23:37:58 +0300

    click animation double effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from 'react'
+import {useSpring,animated} from "react-spring";
+import './styles.css'
 
 export default function App() {
-    return ( <h1> Ebbekir </h1>)
+    const [state, toggle] = useState(true)
+    const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 1000 } })
+    return (
+        <div onClick={() => toggle(!state)}>
+            <animated.div
+                style={{
+                    opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+                    transform: x
+                        .interpolate({
+                            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
+                        })
+                        .interpolate(x => `scale(${x})`)
+                }}>
+                click
+            </animated.div>
+        </div>
+    )
 }
